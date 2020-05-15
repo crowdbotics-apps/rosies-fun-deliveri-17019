b1ac9bfaefcfc2a798cbdcf27f49c70d3c327265
Crowdbotics <team@crowdbotics.com>
2020-05-15 11:33:15 +0000

    Update models

--- backend/rosies_fun_deliveri_17019/wsgi.py
+++ backend/rosies_fun_deliveri_17019/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rosies_fun_deliveri_17019.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "rosies_fun_deliveri_17019.settings")
 
 application = get_wsgi_application()
